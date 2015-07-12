d616b2338a5ea8ab6cc2378ea71fcef45cd4f5ef
Louis Orleans <louis@0rleans.com>
2015-07-12 13:30:43 -0700

    moar code

--- Android/app/src/main/java/io/orleans/smartbox/MainActivity.java
+++ Android/app/src/main/java/io/orleans/smartbox/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -21,11 +22,23 @@ import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.webkit.WebView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 import android.net.wifi.ScanResult;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -44,6 +57,9 @@ public class MainActivity extends AppCompatActivity
 	private CharSequence mTitle;
 	ViewFlipper viewFlipper;
 	WebView webView;
+	ImageView envelope;
+	RelativeLayout letter;
+	TextView letterText;
 
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
@@ -61,9 +77,142 @@ public class MainActivity extends AppCompatActivity
 				(DrawerLayout) findViewById(R.id.drawer_layout));
 
 		webView = (WebView) findViewById(R.id.webView);
+		letter = (RelativeLayout) findViewById(R.id.letter);
+		letterText = (TextView) findViewById(R.id.letterText);
+		envelope = (ImageView) findViewById(R.id.mailStateImg);
 
 		checkIfSettingUpSmartbox();
+
+		updateFromMailbox();
 	}
+
+
+
+
+
+
+
+
+
+
+
+	private void updateFromMailbox () {
+		new DownloadWebpageTask().execute("getData");
+	}
+
+	public class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+		@Override
+		protected String doInBackground(String... urls) {
+
+			// params comes from the execute() call: params[0] is the url.
+			try {
+				return downloadUrl(urls[0]);
+			} catch (IOException e) {
+				return "Unable to retrieve web page. URL may be invalid.";
+			}
+		}
+		// onPostExecute displays the results of the AsyncTask.
+		@Override
+		protected void onPostExecute(String result) {
+			System.out.println(result);
+			JSONObject data;
+			try {
+				data = new JSONObject(result);
+
+				if (data.getBoolean("mail")) {
+					envelope.setBackgroundResource(R.drawable.mail_open);
+					letter.setVisibility(View.VISIBLE);
+				} else {
+					envelope.setBackgroundResource(R.drawable.mail_open);
+					letter.setVisibility(View.INVISIBLE);
+				}
+//				data.getJSONObject("mailPosition") = {front: true, middle:true, back: false};
+//				data.getBoolean("flagUp") = false;
+//				data.getBoolean("doorOpen") = false;
+			} catch (JSONException err) { err.printStackTrace(); }
+
+//			if (result.contains("Turning")) {
+//				txtConnect.setText("Connected");
+//				if (result.contains("on")) {
+//					txtStatus.setText("Sprinklers are on");
+//					imgStatus.setImageResource(R.drawable.watering);
+//				} else if (result.contains("off")) {
+//					txtStatus.setText("Sprinklers are off");
+//					if (result.contains("rain")) {
+//						imgStatus.setImageResource(R.drawable.rain);
+//					} else {
+//						imgStatus.setImageResource(R.drawable.off);
+//					}
+//				}
+//			} else if (result.contains("Status")) {
+//				txtConnect.setText("Connected");
+//				if (result.contains("on")) {
+//					txtStatus.setText("Sprinklers are on");
+//					imgStatus.setImageResource(R.drawable.watering);
+//				} else if (result.contains("off")) {
+//					txtStatus.setText("Sprinklers are off");
+//					if (result.contains("rain")) {
+//						imgStatus.setImageResource(R.drawable.rain);
+//					} else {
+//						imgStatus.setImageResource(R.drawable.off);
+//					}
+//				}
+//			} else if (result.contains("Analytics")) {
+//				txtConnect.setText("Connected");
+//				if (result.contains("watering data")) {
+//
+//				}
+//			}
+		}
+	}
+
+	private String downloadUrl(String myurl) throws IOException {
+		InputStream is = null;
+		// Only display the first 500 characters of the retrieved
+		// web page content.
+		int len = 500;
+
+		try {
+			URL url = new URL("http://joshs-edison.local:8080/" + myurl);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setReadTimeout(10000 /* milliseconds */);
+			conn.setConnectTimeout(15000 /* milliseconds */);
+			conn.setRequestMethod("GET");
+			conn.setDoInput(true);
+			// Starts the query
+			conn.connect();
+			int response = conn.getResponseCode();
+			System.out.println("The response is: " + response);
+			is = conn.getInputStream();
+
+			// Convert the InputStream into a string
+			String contentAsString = readIt(is, len);
+			return contentAsString;
+
+			// Makes sure that the InputStream is closed after the app is
+			// finished using it.
+		} finally {
+			if (is != null) {
+				is.close();
+			}
+		}
+	}
+
+	public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+		Reader reader = null;
+		reader = new InputStreamReader(stream, "UTF-8");
+		char[] buffer = new char[len];
+		reader.read(buffer);
+		return new String(buffer);
+	}
+
+
+
+
+
+
+
+
 
 	private void checkIfSettingUpSmartbox () {
 		final ArrayList<HashMap<String, String>> arraylist = new ArrayList<HashMap<String, String>>();
@@ -76,18 +225,11 @@ public class MainActivity extends AppCompatActivity
 			@Override
 			public void onReceive (Context c, Intent intent) {
 				List<ScanResult>results = wifi.getScanResults();
-				System.out.println(results);
 				int size = results.size();
 
 				try {
 					size = size - 1;
 					while (size >= 0) {
-//						HashMap<String, String> item = new HashMap<String, String>();
-//						item.put("key", results.get(size).SSID + "  " + results.get(size).capabilities);
-
-
-						System.out.println("SSID: " + results.get(size).SSID);
-
 						if (results.get(size).SSID.equals("Smartbox")) {
 //						if (results.get(size).SSID.equals("Intel 5GHz")) {
 							System.out.println("FOUND WIFI");
@@ -110,25 +252,6 @@ public class MainActivity extends AppCompatActivity
 			}
 		}, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
 		wifi.startScan();
-//		Toast.makeText(this, "Scanning...." + size, Toast.LENGTH_SHORT).show();
-
-
-
-
-
-
-
-
-//		getlistOfWifi();
-//		foreach (wifinetwork) {
-//			if (networkName.equals("Smartbox")) {
-//				String ip = Utils.getIPAddress(true);;
-//				String[] splitIP = ip.split(".");
-//				splitIP[3] = "1";
-//				ip = splitIP[0] + "." + splitIP[1] + "." + splitIP[2] + "." + splitIP[3];
-//				webView.loadUrl("http://" + ip + ":81");
-//			}
-//		}
 	}
 
 	@Override
