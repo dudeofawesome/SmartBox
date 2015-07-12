74c3bf36e013376452fefe258cc837ddff051777
Louis Orleans <louis@0rleans.com>
2015-07-12 11:33:14 -0700

    app update

--- Android/app/src/main/java/io/orleans/smartbox/MainActivity.java
+++ Android/app/src/main/java/io/orleans/smartbox/MainActivity.java
@@ -1,6 +1,10 @@
 package io.orleans.smartbox;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -15,8 +19,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
+import android.webkit.WebView;
 import android.widget.ArrayAdapter;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
+import android.net.wifi.ScanResult;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 		implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -30,6 +42,8 @@ public class MainActivity extends AppCompatActivity
 	 * Used to store the last screen title. For use in {@link #restoreActionBar()}.
 	 */
 	private CharSequence mTitle;
+	ViewFlipper viewFlipper;
+	WebView webView;
 
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
@@ -39,11 +53,82 @@ public class MainActivity extends AppCompatActivity
 		mNavigationDrawerFragment = (NavigationDrawerFragment)
 				getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
 		mTitle = getTitle();
+		viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper);
 
 		// Set up the drawer.
 		mNavigationDrawerFragment.setUp(
 				R.id.navigation_drawer,
 				(DrawerLayout) findViewById(R.id.drawer_layout));
+
+		webView = (WebView) findViewById(R.id.webView);
+
+		checkIfSettingUpSmartbox();
+	}
+
+	private void checkIfSettingUpSmartbox () {
+		final ArrayList<HashMap<String, String>> arraylist = new ArrayList<HashMap<String, String>>();
+		final WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		if (wifi.isWifiEnabled() == false) {
+			wifi.setWifiEnabled(true);
+		}
+
+		registerReceiver(new BroadcastReceiver() {
+			@Override
+			public void onReceive (Context c, Intent intent) {
+				List<ScanResult>results = wifi.getScanResults();
+				System.out.println(results);
+				int size = results.size();
+
+				try {
+					size = size - 1;
+					while (size >= 0) {
+//						HashMap<String, String> item = new HashMap<String, String>();
+//						item.put("key", results.get(size).SSID + "  " + results.get(size).capabilities);
+
+
+						System.out.println("SSID: " + results.get(size).SSID);
+
+						if (results.get(size).SSID.equals("Smartbox")) {
+//						if (results.get(size).SSID.equals("Intel 5GHz")) {
+							System.out.println("FOUND WIFI");
+							String ip = Utils.getIPAddress(true);
+							String[] splitIP = ip.split("\\.");
+							splitIP[3] = "1";
+							ip = splitIP[0] + "." + splitIP[1] + "." + splitIP[2] + "." + splitIP[3];
+							webView.loadUrl("http://" + ip + ":81");
+
+							System.out.println("IP4: " + ip);
+
+							break;
+						}
+
+						size--;
+					}
+				} catch (Exception e) {
+
+				}
+			}
+		}, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+		wifi.startScan();
+//		Toast.makeText(this, "Scanning...." + size, Toast.LENGTH_SHORT).show();
+
+
+
+
+
+
+
+
+//		getlistOfWifi();
+//		foreach (wifinetwork) {
+//			if (networkName.equals("Smartbox")) {
+//				String ip = Utils.getIPAddress(true);;
+//				String[] splitIP = ip.split(".");
+//				splitIP[3] = "1";
+//				ip = splitIP[0] + "." + splitIP[1] + "." + splitIP[2] + "." + splitIP[3];
+//				webView.loadUrl("http://" + ip + ":81");
+//			}
+//		}
 	}
 
 	@Override
@@ -59,9 +144,11 @@ public class MainActivity extends AppCompatActivity
 		switch (number) {
 			case 1:
 				mTitle = getString(R.string.title_section1);
+				viewFlipper.setDisplayedChild(1);
 				break;
 			case 2:
 				mTitle = getString(R.string.title_section2);
+				viewFlipper.setDisplayedChild(0);
 				break;
 		}
 	}
@@ -125,13 +212,6 @@ public class MainActivity extends AppCompatActivity
 		}
 
 		public PlaceholderFragment () {
-		}
-
-		@Override
-		public View onCreateView (LayoutInflater inflater, ViewGroup container,
-		                          Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-			return rootView;
 		}
 
 		@Override
